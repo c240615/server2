7ab410f8e038b1bc66c95ef08b53fa9e545aa6cb
c240615 <97713225+c240615@users.noreply.github.com>
2022-04-14 14:07:13 +0800

    first commit

---
+++ src/註冊表單/interface/task.ts
@@ -0,0 +1,9 @@
+// 輸出模塊、變量接口 import 加載export街口的模塊
+export interface task{
+    userInfo:{        
+        id:number,
+        tasks:string,
+        day:string,
+        reminder:boolean,
+    }
+}
